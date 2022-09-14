c8016403d39c49519edc68cabf89d4b95c162ce5
codev911 <codev911@gmail.com>
2022-09-14 23:12:12 +0700

    update main.ts

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const swagger_1 = require("@nestjs/swagger");
+const path_1 = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const config = new swagger_1.DocumentBuilder()
@@ -13,6 +14,9 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api-docs', app, document);
+    app.useStaticAssets((0, path_1.join)(__dirname, '/api-docs'), {
+        prefix: '/api-docs',
+    });
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Solidity Parser')
@@ -13,6 +15,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
+
+  app.useStaticAssets(join(__dirname, '/api-docs'), {
+    prefix: '/api-docs',
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
